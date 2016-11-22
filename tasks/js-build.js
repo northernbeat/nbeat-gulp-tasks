7b3b1a4bc518437cda17840c98f72147a0296a51
Eirik Refsdal <eirikref@gmail.com>
2016-11-22 16:33:04 +0100

    Sync

--- tasks/js-build.js
+++ tasks/js-build.js
@@ -9,19 +9,17 @@ module.exports = function(gulp, plugins, config)
         
         for (target in config.build.js) {
             if (config["build"]["js"][target]["src"] instanceof Array && config["build"]["js"][target]["dest"]) {
-                config["build"]["js"][target]["src"].forEach(function(a) {
-                    return gulp.src(a)
-                        .pipe(plugins.concat(config["build"]["js"][target]["dest"]))
-                        .pipe(gulp.dest(config.build.dest))
-                        .pipe(plugins.debug({title: "JS, out:"}))
-
-                        .pipe(plugins.uglify())
-                        .pipe(plugins.rename(function(path) {
-                            path.basename += ".min";
-                        }))
-                        .pipe(gulp.dest(config.build.dest))
-                        .pipe(plugins.debug({title: "JS, out:"}))
-                });
+                return gulp.src(config["build"]["js"][target]["src"])
+                    .pipe(plugins.concat(config["build"]["js"][target]["dest"]))
+                    .pipe(gulp.dest(config.build.dest))
+                    .pipe(plugins.debug({title: "JS, out:"}))
+                
+                // .pipe(plugins.uglify())
+                // .pipe(plugins.rename(function(path) {
+                // path.basename += ".min";
+                // }))
+                // .pipe(gulp.dest(config.build.dest))
+                // .pipe(plugins.debug({title: "JS, out:"}))
             }
         }
     }
